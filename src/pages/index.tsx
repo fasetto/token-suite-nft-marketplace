858e06249c39459b9be6a356efa44c88c905f6ce
Serkan Bircan <serkanbircan21@yandex.com>
2021-10-10 14:07:25 +0300

    Add filtering support

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Head from "next/head";
 
-import { Heading, NextImage, NFTItem } from "@/components";
+import { Heading, NextImage, NFTItem, Selectbox } from "@/components";
+import { NFTItem as NFTItemType } from "@/graphql/queries";
 import { useCategories, useNFTItems } from "@/hooks";
 
 export default function Home() {
   const { data, loading } = useNFTItems();
   const { data: categoryData } = useCategories();
 
-  console.log(data?.cardEntities);
+  const [nftItems, dispatchNFTItems] = useState<NFTItemType[]>();
+  const [selectedCategory, dispatchSelectedCategory] = useState<string>();
+
+  useEffect(() => {
+    let filteredItems = data?.cardEntities.filter(
+      x => x.category.name === selectedCategory
+    );
+
+    if (!selectedCategory || selectedCategory === "category")
+      filteredItems = data?.cardEntities;
+
+    dispatchNFTItems(filteredItems);
+  }, [selectedCategory, data?.cardEntities]);
 
   return (
     <>
@@ -30,24 +43,36 @@ export default function Home() {
           NFT Marketplace
         </Heading>
 
-        <div className="nft-items">
-          {!loading &&
-            data?.cardEntities.map(item => (
-              <NFTItem
-                key={item.id}
-                name={item.name}
-                image={item.image}
-                category={item.category.name}
-                power={item.weight}
-                ethPrice={0}
-                usdPrice={0}
+        <div className="homepage__wrapper">
+          <div className="homepage__filter-dropdown">
+            <label>
+              <span>Filter by:</span>
+              <Selectbox
+                items={categoryData?.categoryEntities.map(x => x.name)}
+                onChange={e => dispatchSelectedCategory(e.target.value)}
               />
-            ))}
+            </label>
+          </div>
 
-          {loading &&
-            Array.from({ length: 9 }).map((_, i) => (
-              <NFTItem.Skeleton key={i} />
-            ))}
+          <div className="homepage__nft-items">
+            {!loading &&
+              nftItems?.map(item => (
+                <NFTItem
+                  key={item.id}
+                  name={item.name}
+                  image={item.image}
+                  category={item.category.name}
+                  power={item.weight}
+                  ethPrice={0}
+                  usdPrice={0}
+                />
+              ))}
+
+            {loading &&
+              Array.from({ length: 9 }).map((_, i) => (
+                <NFTItem.Skeleton key={i} />
+              ))}
+          </div>
         </div>
       </div>
     </>

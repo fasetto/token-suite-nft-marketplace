74657b860fff055e19b35d6f0a4e23c147d3077c
Serkan Bircan <serkanbircan21@yandex.com>
2021-10-10 14:07:13 +0300

    Export selectbox

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Heading } from "./Heading";
 export { default as NFTItem } from "./NFTItem";
 export { default as NextImage } from "./NextImage";
 export { default as Button } from "./Button";
+export { default as Selectbox } from "./Selectbox";

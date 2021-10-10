9d0758fed4ccea9c644cca60ff58a60408d59778
Serkan Bircan <serkanbircan21@yandex.com>
2021-10-10 12:28:36 +0300

    Add homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,43 +1,55 @@
+import React from "react";
 import Head from "next/head";
 
+import { Heading, NextImage, NFTItem } from "@/components";
+import { useCategories, useNFTItems } from "@/hooks";
+
 export default function Home() {
+  const { data, loading } = useNFTItems();
+  const { data: categoryData } = useCategories();
+
+  console.log(data?.cardEntities);
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <>
       <Head>
-        <title>Next JS Template</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>NFT Marketplace &ndash; TokenSuite</title>
       </Head>
 
-      <main className="flex flex-col items-center flex-1 mt-8 px-20 text-center">
-        <h1 className="text-5xl font-bold">
-          Welcome to{" "}
-          <a
-            className="text-blue-600 hover:underline"
-            href="https://nextjs.org"
-          >
-            Next.js!
-          </a>
-        </h1>
+      <div className="homepage">
+        <div className="homepage__logo">
+          <NextImage
+            src="/logo.svg"
+            layout="fill"
+            alt="TokenSuite logo"
+            priority
+          />
+        </div>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{" "}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-      </main>
+        <Heading as="h1" className="homepage__title">
+          NFT Marketplace
+        </Heading>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
-    </div>
+        <div className="nft-items">
+          {!loading &&
+            data?.cardEntities.map(item => (
+              <NFTItem
+                key={item.id}
+                name={item.name}
+                image={item.image}
+                category={item.category.name}
+                power={item.weight}
+                ethPrice={0}
+                usdPrice={0}
+              />
+            ))}
+
+          {loading &&
+            Array.from({ length: 9 }).map((_, i) => (
+              <NFTItem.Skeleton key={i} />
+            ))}
+        </div>
+      </div>
+    </>
   );
 }
